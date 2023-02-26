b94a4355b784cdd641709488fcdd8423e98c55d9
RogerQi <rizhaoq2@illinois.edu>
2023-02-25 18:25:31 -0600

    remove debugging main/iostream that caused compilation errors

--- shared/libraries/crc8.h
+++ shared/libraries/crc8.h
@@ -31,7 +31,7 @@
  * @param  ucCRC8     Initialized checksum
  * @return            CRC checksum
  */
-static uint8_t get_crc8_check_sum(uint8_t* pchMessage, uint16_t dwLength, uint8_t ucCRC8);
+uint8_t get_crc8_check_sum(uint8_t* pchMessage, uint16_t dwLength, uint8_t ucCRC8);
 
 /**
  * Verify CRC8
