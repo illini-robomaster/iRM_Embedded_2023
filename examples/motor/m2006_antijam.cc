54d39d7221b71faf04aa1a06d13a3a6e17f28a44
N9nGe <muxin@wirelessprv-10-192-83-125.near.illinois.edu>
2023-03-11 18:16:24 -0600

    still test antijam

---
+++ examples/motor/m2006_antijam.cc
@@ -0,0 +1,95 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2022 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#include "bsp_gpio.h"
+#include "bsp_os.h"
+#include "bsp_print.h"
+#include "cmsis_os.h"
+#include "controller.h"
+#include "main.h"
+#include "motor.h"
+#include "utils.h"
+#include <unistd.h>
+
+
+#define KEY_GPIO_GROUP GPIOB
+#define KEY_GPIO_PIN GPIO_PIN_2
+
+#define NOTCH (2 * PI / 4)
+#define SPEED (2 * PI)
+#define ACCELERATION (8 * PI)
+
+bsp::CAN* can1 = nullptr;
+control::MotorCANBase* motor = nullptr;
+control::ServoMotor* servo = nullptr;
+BoolEdgeDetector key_detector(false);
+
+void jam_callback(control::ServoMotor* servo, const control::servo_jam_t data) {
+  UNUSED(data);
+  float servo_target = servo->GetTarget();
+  if (servo_target < servo->GetTheta()) {
+    print("Antijam in operation\r\n");
+  } else {
+    float prev_target = servo->GetTarget() - NOTCH;
+    servo->SetTarget(prev_target, true);
+    print("Antijam engage\r\n");
+  }
+}
+
+void RM_RTOS_Init() {
+  // print_use_uart(&huart8);
+  bsp::SetHighresClockTimer(&htim2);
+
+  can1 = new bsp::CAN(&hcan1, 0x201);
+  motor = new control::Motor2006(can1, 0x202);
+
+  control::servo_t servo_data;
+  servo_data.motor = motor;
+  servo_data.max_speed = SPEED;
+  servo_data.max_acceleration = ACCELERATION;
+  servo_data.transmission_ratio = M2006P36_RATIO;
+  servo_data.omega_pid_param = new float[3]{150, 4, 0};
+  servo_data.max_iout = 2000;
+  servo_data.max_out = 10000;
+  servo = new control::ServoMotor(servo_data);
+
+  servo->RegisterJamCallback(jam_callback, 0.6);
+}
+
+void RM_RTOS_Default_Task(const void* args) {
+  UNUSED(args);
+
+  control::MotorCANBase* motors[] = {motor};
+  bsp::GPIO key(KEY_GPIO_GROUP, KEY_GPIO_PIN);
+
+  while (true) {
+    //key_detector.input(key.Read());
+    //if (key_detector.posEdge() && servo->SetTarget(servo->GetTarget() + NOTCH) != 0) {
+    // const TickType_t tDelay = 1000;
+    // vTaskDelay(tDelay);
+    // sleep(1);
+    if (servo->SetTarget(servo->GetTarget() + NOTCH) != 0) {
+      print("Servomotor step forward, target: %8.4f\r\n", servo->GetTarget());
+    }
+    servo->CalcOutput();
+    control::MotorCANBase::TransmitOutput(motors, 1);
+    osDelay(500);
+  }
+}
