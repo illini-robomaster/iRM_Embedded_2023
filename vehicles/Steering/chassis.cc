4fd28c7174c511785ac9262150e7c47f8f4b73c2
youhy <haoyuan2019@outlook.com>
2023-04-15 19:41:40 -0700

    fix steering spinning bug

--- vehicles/Steering/chassis.cc
+++ vehicles/Steering/chassis.cc
@@ -30,6 +30,7 @@
 #include "protocol.h"
 #include "rgb.h"
 #include "steering.h"
+#include <cmath>
 
 static bsp::CAN* can1 = nullptr;
 static bsp::CAN* can2 = nullptr;
@@ -46,9 +47,14 @@ static const int KILLALL_DELAY = 100;
 static const int DEFAULT_TASK_DELAY = 100;
 static const int CHASSIS_TASK_DELAY = 2;
 
+// speed for steering motors (rad/s)
 constexpr float RUN_SPEED = (4 * PI);
 constexpr float ALIGN_SPEED = (PI);
 constexpr float ACCELERATION = (100 * PI);
+
+// speed for chassis rotation (no unit)
+constexpr float SPIN_SPEED = 80;
+constexpr float FOLLOW_SPEED = 40;
 
 //==================================================================================================
 // Referee
@@ -145,13 +151,10 @@ void chassisTask(void* arg) {
   control::MotorCANBase* steer_motors[] = {motor1, motor2, motor3, motor4};
   control::MotorCANBase* wheel_motors[] = {motor5, motor6, motor7, motor8};
 
-  control::PIDController pid5(120, 15, 30);
-  control::PIDController pid6(120, 15, 30);
-  control::PIDController pid7(120, 15, 30);
-  control::PIDController pid8(120, 15, 30);
-
-  float spin_speed = 10;
-  float follow_speed = 10;
+  control::PIDController pid5(120, 15, 0);
+  control::PIDController pid6(120, 15, 0);
+  control::PIDController pid7(120, 15, 0);
+  control::PIDController pid8(120, 15, 0);
 
   while (!receive->start) osDelay(100);
 
@@ -172,31 +175,37 @@ void chassisTask(void* arg) {
   chassis->SteerThetaReset();
   chassis->SetWheelSpeed(0,0,0,0);
 
-
   while (true) {
     float relative_angle = receive->relative_angle;
-    float sin_yaw, cos_yaw, vx_set, vy_set, wz_set;
-    UNUSED(wz_set);
+    float sin_yaw, cos_yaw, vx_set, vy_set;
+    float vx, vy, wz;
 
-    vx_set = receive->vx;
-    vy_set = receive->vy;
+    // TODO need to change the channels in gimbal.cc
+    vx_set = -receive->vy;
+    vy_set = receive->vx;
 
     if (receive->mode == 1) {  // spin mode
-      sin_yaw = arm_sin_f32(relative_angle);
-      cos_yaw = arm_cos_f32(relative_angle);
-      vx_set = cos_yaw * vx_set + sin_yaw * vy_set;
-      vy_set = -sin_yaw * vx_set + cos_yaw * vy_set;
-      wz_set = spin_speed;
+      // delay compensation
+      // based on rule-of-thumb formula SPIN_SPEED = 80 = ~30 degree of error
+      relative_angle = relative_angle - PI * 30.0 / 180.0 / 80.0 * SPIN_SPEED;
+
+      chassis->SteerSetMaxSpeed(RUN_SPEED * 2);
+      sin_yaw = sin(relative_angle);
+      cos_yaw = cos(relative_angle);
+      vx = cos_yaw * vx_set + sin_yaw * vy_set;
+      vy = -sin_yaw * vx_set + cos_yaw * vy_set;
+      wz = SPIN_SPEED;
     } else {
-      sin_yaw = arm_sin_f32(relative_angle);
-      cos_yaw = arm_cos_f32(relative_angle);
-      vx_set = cos_yaw * vx_set + sin_yaw * vy_set;
-      vy_set = -sin_yaw * vx_set + cos_yaw * vy_set;
-      wz_set = std::min(follow_speed, follow_speed * relative_angle);
-      if (-CHASSIS_DEADZONE < relative_angle && relative_angle < CHASSIS_DEADZONE) wz_set = 0;
+      chassis->SteerSetMaxSpeed(RUN_SPEED);
+      sin_yaw = sin(relative_angle);
+      cos_yaw = cos(relative_angle);
+      vx = cos_yaw * vx_set + sin_yaw * vy_set;
+      vy = -sin_yaw * vx_set + cos_yaw * vy_set;
+      wz = std::min(FOLLOW_SPEED, FOLLOW_SPEED * relative_angle);
+      if (-CHASSIS_DEADZONE < relative_angle && relative_angle < CHASSIS_DEADZONE) wz = 0;
     }
 
-    chassis->SetSpeed(vx_set / 10, vy_set / 10, wz_set * 10);
+    chassis->SetSpeed(vx / 10, vy / 10, wz);
     chassis->SteerUpdateTarget();
     constexpr float WHEEL_SPEED_FACTOR = 4;
     chassis->WheelUpdateSpeed(WHEEL_SPEED_FACTOR);
@@ -281,7 +290,7 @@ void RM_RTOS_Init() {
   steering_motor_data.max_speed = RUN_SPEED;
   steering_motor_data.max_acceleration = ACCELERATION;
   steering_motor_data.transmission_ratio = 8;
-  steering_motor_data.omega_pid_param = new float[3]{140, 1.2, 25};
+  steering_motor_data.omega_pid_param = new float[3]{140, 1.2, 0};
   steering_motor_data.max_iout = 1000;
   steering_motor_data.max_out = 13000;
   steering_motor_data.calibrate_offset = 0;
