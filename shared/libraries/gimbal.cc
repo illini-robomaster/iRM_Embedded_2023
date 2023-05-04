51783c70ab4386deca8f92371e891a53e2d5cb07
jerrywrx <48041557+jerrywrx@users.noreply.github.com>
2023-05-04 14:58:43 -0500

    Jerrywrx/4310 upload (#28)
    * working on 4310 driver
    
    * m4310 works; needs parameter tuning and figure out master & slave id
    
    * added two motors robot arm control
    
    * working on 4310
    
    * able to transmit params in float; need testing on vel
    
    * reset util
    
    * reset m3508_speed
    
    * three different mode works
    
    * added m4310 examples
    
    * finished update data; need testing
    
    * MIT mode works on gimbal; pos-vel mode has vibration issues
    
    * tested m4310 on gimbal main program and it works; need to add soft start
    
    * fixing soft kill; need testing
    
    * soft start and soft kill works
    
    * removed m3508_steering_align.cc from cmakelist
    
    * fixed changes suggested by yhy; reconstructed 4310 motor class
    
    * made changes suggested by yhy
    
    * added delay after pitch motor is enabled; gave magic numbers names
    
    * fixed issues suggested by yhy
    
    * fixed inconsistent type

--- shared/libraries/gimbal.cc
+++ shared/libraries/gimbal.cc
@@ -8,6 +8,7 @@ namespace control {
 Gimbal::Gimbal(gimbal_t gimbal)
     : pitch_detector_(BoolEdgeDetector(false)), yaw_detector_(BoolEdgeDetector(false)) {
   // acquired from user
+  pitch_motor_4310_ = gimbal.pitch_motor_4310_;
   pitch_motor_ = gimbal.pitch_motor;
   yaw_motor_ = gimbal.yaw_motor;
   model_ = gimbal.model;
@@ -84,14 +85,40 @@ Gimbal::Gimbal(gimbal_t gimbal)
         yaw_omega_pid_param_ = new float[3]{3600, 20, 0};
       }
       break;
+    case GIMBAL_STEERING_4310:
+      //      data_.yaw_offset_ = 0.3451;
+      data_.yaw_offset_ = 4.52;
+      data_.yaw_max_ = PI;
+      {
+        yaw_theta_max_iout = 0;
+        yaw_theta_max_out = 10;
+        yaw_omega_max_iout = 10000;  // 10000
+        yaw_omega_max_out = 30000;
+
+        yaw_theta_pid_param_ = new float[3]{30, 0, 0.3};
+        yaw_omega_pid_param_ = new float[3]{3600, 20, 0};
+        yaw_theta_pid_ =
+            new ConstrainedPID(yaw_theta_pid_param_, yaw_theta_max_iout, yaw_theta_max_out);
+        yaw_omega_pid_ =
+            new ConstrainedPID(yaw_omega_pid_param_, yaw_omega_max_iout, yaw_omega_max_out);
+      }
+      break;
     default:
       RM_ASSERT_TRUE(false, "Not Supported Gimbal Mode\r\n");
   }
 
   // nullptr guard
-  if (pitch_theta_pid_param_ == nullptr || pitch_omega_pid_param_ == nullptr ||
-      yaw_theta_pid_param_   == nullptr || yaw_omega_pid_param_ == nullptr) {
-      RM_ASSERT_TRUE(false, "Not Supported Gimbal Mode\r\n");
+  switch (gimbal.model) {
+    case GIMBAL_STEERING_4310:
+      if (yaw_theta_pid_param_   == nullptr || yaw_omega_pid_param_ == nullptr) {
+        RM_ASSERT_TRUE(false, "Not Supported Gimbal Mode\r\n");
+      }
+      break;
+    default:
+      if (pitch_theta_pid_param_ == nullptr || pitch_omega_pid_param_ == nullptr ||
+          yaw_theta_pid_param_   == nullptr || yaw_omega_pid_param_ == nullptr) {
+        RM_ASSERT_TRUE(false, "Not Supported Gimbal Mode\r\n");
+      }
   }
 
   pitch_theta_pid_ =
@@ -113,46 +140,81 @@ Gimbal::Gimbal(gimbal_t gimbal)
 }
 
 Gimbal::~Gimbal() {
-  delete pitch_theta_pid_param_;
-  pitch_theta_pid_param_ = nullptr;
-  delete pitch_omega_pid_param_;
-  pitch_omega_pid_param_ = nullptr;
-  delete yaw_theta_pid_param_;
-  yaw_theta_pid_param_ = nullptr;
-  delete yaw_omega_pid_param_;
-  yaw_omega_pid_param_ = nullptr;
-  delete pitch_theta_pid_;
-  pitch_theta_pid_ = nullptr;
-  delete pitch_omega_pid_;
-  pitch_omega_pid_ = nullptr;
-  delete yaw_theta_pid_;
-  yaw_theta_pid_ = nullptr;
-  delete yaw_omega_pid_;
-  yaw_omega_pid_ = nullptr;
+  switch (model_) {
+    case GIMBAL_STEERING_4310: {
+      delete yaw_theta_pid_param_;
+      yaw_theta_pid_param_ = nullptr;
+      delete yaw_omega_pid_param_;
+      yaw_omega_pid_param_ = nullptr;
+      delete yaw_theta_pid_;
+      yaw_theta_pid_ = nullptr;
+      delete yaw_omega_pid_;
+      yaw_omega_pid_ = nullptr;
+      break;
+    }
+    default:
+      delete pitch_theta_pid_param_;
+      pitch_theta_pid_param_ = nullptr;
+      delete pitch_omega_pid_param_;
+      pitch_omega_pid_param_ = nullptr;
+      delete yaw_theta_pid_param_;
+      yaw_theta_pid_param_ = nullptr;
+      delete yaw_omega_pid_param_;
+      yaw_omega_pid_param_ = nullptr;
+      delete pitch_theta_pid_;
+      pitch_theta_pid_ = nullptr;
+      delete pitch_omega_pid_;
+      pitch_omega_pid_ = nullptr;
+      delete yaw_theta_pid_;
+      yaw_theta_pid_ = nullptr;
+      delete yaw_omega_pid_;
+      yaw_omega_pid_ = nullptr;
+  }
 }
 
 gimbal_data_t* Gimbal::GetData() { return &data_; }
 
 void Gimbal::Update() {
-  float pt_diff = pitch_motor_->GetThetaDelta(pitch_angle_);
-  float pt_out = pitch_theta_pid_->ComputeOutput(pt_diff);
-  float po_in = pitch_motor_->GetOmegaDelta(pt_out);
-  float po_out = pitch_omega_pid_->ComputeConstrainedOutput(po_in);
+  switch (model_) {
+    case GIMBAL_STEERING_4310: {
+      float yt_diff = yaw_motor_->GetThetaDelta(yaw_angle_);
+      float yt_out = yaw_theta_pid_->ComputeOutput(yt_diff);
+      float yt_in = yaw_motor_->GetOmegaDelta(yt_out);
+      float yo_out = yaw_omega_pid_->ComputeConstrainedOutput(yt_in);
 
-  float yt_diff = yaw_motor_->GetThetaDelta(yaw_angle_);
-  float yt_out = yaw_theta_pid_->ComputeOutput(yt_diff);
-  float yt_in = yaw_motor_->GetOmegaDelta(yt_out);
-  float yo_out = yaw_omega_pid_->ComputeConstrainedOutput(yt_in);
+      yaw_motor_->SetOutput(yo_out);
+      break;
+    }
+    default:
+      float pt_diff = pitch_motor_->GetThetaDelta(pitch_angle_);
+      float pt_out = pitch_theta_pid_->ComputeOutput(pt_diff);
+      float po_in = pitch_motor_->GetOmegaDelta(pt_out);
+      float po_out = pitch_omega_pid_->ComputeConstrainedOutput(po_in);
 
-  pitch_motor_->SetOutput(po_out);
-  yaw_motor_->SetOutput(yo_out);
+      float yt_diff = yaw_motor_->GetThetaDelta(yaw_angle_);
+      float yt_out = yaw_theta_pid_->ComputeOutput(yt_diff);
+      float yt_in = yaw_motor_->GetOmegaDelta(yt_out);
+      float yo_out = yaw_omega_pid_->ComputeConstrainedOutput(yt_in);
+
+      pitch_motor_->SetOutput(po_out);
+      yaw_motor_->SetOutput(yo_out);
+  }
 }
 
 void Gimbal::TargetAbsWOffset(float abs_pitch, float abs_yaw) {
-  float clipped_pitch = clip<float>(abs_pitch, -data_.pitch_max_, data_.pitch_max_);
-  float clipped_yaw = clip<float>(abs_yaw, -data_.yaw_max_, data_.yaw_max_);
-  pitch_angle_ = wrapping_clip<float>(clipped_pitch + data_.pitch_offset_, pitch_lower_limit_, pitch_upper_limit_, 0, 2 * PI);
-  yaw_angle_ = wrapping_clip<float>(clipped_yaw + data_.yaw_offset_, yaw_lower_limit_, yaw_upper_limit_, 0, 2 * PI);
+  switch (model_) {
+    case GIMBAL_STEERING_4310: {
+      float clipped_yaw = clip<float>(abs_yaw, -data_.yaw_max_, data_.yaw_max_);
+      yaw_angle_ = wrap<float>(clipped_yaw + data_.yaw_offset_, 0, 2 * PI);
+      break;
+    }
+    default:
+      float clipped_pitch = clip<float>(abs_pitch, -data_.pitch_max_, data_.pitch_max_);
+      float clipped_yaw = clip<float>(abs_yaw, -data_.yaw_max_, data_.yaw_max_);
+      pitch_angle_ = wrapping_clip<float>(clipped_pitch + data_.pitch_offset_, pitch_lower_limit_,
+                                          pitch_upper_limit_, 0, 2 * PI);
+      yaw_angle_ = wrapping_clip<float>(clipped_yaw + data_.yaw_offset_, yaw_lower_limit_,yaw_upper_limit_, 0, 2 * PI);
+  }
 }
 
 void Gimbal::TargetAbsNoOffset(float abs_pitch, float abs_yaw) {
@@ -161,8 +223,15 @@ void Gimbal::TargetAbsNoOffset(float abs_pitch, float abs_yaw) {
 }
 
 void Gimbal::TargetRel(float rel_pitch, float rel_yaw) {
-  pitch_angle_ = ComputePitchRel(rel_pitch, pitch_angle_);
-  yaw_angle_ = ComputeYawRel(rel_yaw, yaw_angle_);
+  switch (model_) {
+    case GIMBAL_STEERING_4310: {
+      yaw_angle_ = yaw_motor_->GetTheta() + rel_yaw;
+      break;
+    }
+    default:
+      pitch_angle_ = ComputePitchRel(rel_pitch, pitch_angle_);
+      yaw_angle_ = ComputeYawRel(rel_yaw, yaw_angle_);
+  }
 }
 
 float Gimbal::ComputePitchRel(float rel_pitch, float ref_pitch) {
