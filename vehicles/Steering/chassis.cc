4de6bf99113b050d1bec24db409ac3ad0e6cc813
N9nGe <muxin@GN9ne.local>
2023-07-05 13:39:35 +0800

    fix the coding format continue

--- vehicles/Steering/chassis.cc
+++ vehicles/Steering/chassis.cc
@@ -270,18 +270,18 @@ void chassisTask(void* arg) {
 
     //consider using uart printing to check the power limit's value
     //log values out as files to obtain its trend
-    if(supercap_voltage<=1){
+    if(supercap_voltage<=1) {
       //edge case to prevent power limit from being negative
       chassis->Update(10.0,
                       referee->power_heat_data.chassis_power,
                       (float)referee->power_heat_data.chassis_power_buffer);
-    } else if (supercap_voltage <= 5.0||supercap_voltage>1.0){
+    } else if (supercap_voltage>1.0 || supercap_voltage <= 5.0) {
       //Ensure chassis won't use too much power causing losing hp when revive
       //When voltage is lower than 1, so power should be used
       chassis->Update((supercap_voltage-1)*7.5,
                       referee->power_heat_data.chassis_power,
                       (float)referee->power_heat_data.chassis_power_buffer);
-    } else{
+    } else {
       //linear function for super capacitor's voltage above 5V
       chassis->Update(((float)25+((float)supercap_voltage-5.0)*2.0),
                       referee->power_heat_data.chassis_power,
