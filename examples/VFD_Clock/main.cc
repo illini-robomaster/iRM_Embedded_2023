11c862eea4a20555185257aae5233cac44a6bc4f
AlchemicRonin <1216532005@qq.com>
2023-07-10 17:38:37 +0800

    [chore] try to use Scopy to verify delay_us function

--- examples/VFD_Clock/main.cc
+++ examples/VFD_Clock/main.cc
@@ -26,7 +26,83 @@
 #include "utils.h"
 
 #include "bsp_gpio.h"
-#include "vfd.h"
+
+void delay_us(uint32_t us) {
+  uint32_t start = bsp::GetHighresTickMicroSec();
+  while (bsp::GetHighresTickMicroSec() - start < us);
+}
+
+namespace display {
+
+class VFD {
+ public:
+  VFD(bsp::GPIO* din, bsp::GPIO* clk, bsp::GPIO* cs, bsp::GPIO* rst, bsp::GPIO* en) {
+    din_ = din;
+    clk_ = clk;
+    cs_ = cs;
+    rst_ = rst;
+    en_ = en;
+
+    en_->High();
+    delay_us(100);
+    rst_->Low();
+    osDelay(5);
+    rst_->High();
+  }
+
+  void Show() {
+    cs_->Low();
+    write_data(0xe8);
+    cs_->High();
+  }
+
+  void Init() {
+    cs_->Low();
+    write_data(0xe0);
+    osDelay(5);
+    write_data(0x07);
+    cs_->High();
+    osDelay(5);
+
+    cs_->Low();
+    write_data(0xe4);
+    osDelay(5);
+    write_data(0xff);
+    cs_->High();
+    osDelay(5);
+  }
+
+  void WriteOneChar(unsigned char x, unsigned char chr) {
+    cs_->Low();
+    write_data(0x20 + x);
+    write_data(chr);
+    cs_->High();
+    Show();
+  }
+
+ private:
+  bsp::GPIO* din_;
+  bsp::GPIO* clk_;
+  bsp::GPIO* cs_;
+  bsp::GPIO* rst_;
+  bsp::GPIO* en_;
+
+  void write_data(unsigned char data) {
+    for (int i = 0; i < 8; ++i) {
+      if ((data & 0x01) == 0x01)
+        din_->High();
+      else
+        din_->Low();
+      clk_->Low();
+      delay_us(5);
+      data >>= 1;
+      clk_->High();
+      delay_us(5);
+    }
+  }
+};
+
+}
 
 static bsp::GPIO* led = nullptr;
 
@@ -45,13 +121,9 @@ static BoolEdgeDetector left(false);
 static BoolEdgeDetector button(false);
 static BoolEdgeDetector right(false);
 
-void delay_us(uint32_t us) {
-  uint32_t start = bsp::GetHighresTickMicroSec();
-  while (bsp::GetHighresTickMicroSec() - start < us);
-}
-
 void RM_RTOS_Init(void) {
-  bsp::SetHighresClockTimer(&htim3);
+  bsp::SetHighresClockTimer(&htim2);
+  print_use_uart(&huart1);
 
   led = new bsp::GPIO(LED_GPIO_Port, LED_Pin);
 
@@ -69,6 +141,7 @@ void RM_RTOS_Init(void) {
 
 void RM_RTOS_Default_Task(const void* arguments) {
   UNUSED(arguments);
+//  vfd->Init();
 
   while (true) {
     left.input(ccw->Read());
@@ -77,12 +150,17 @@ void RM_RTOS_Default_Task(const void* arguments) {
 
     if (left.negEdge() || button.negEdge() || right.negEdge())
       led->Toggle();
-//    osDelay(1);
-    led->Low();
-//    osDelay(1000);
-    delay_us(1000000);
-    led->High();
-    osDelay(1000);
-//    osDelay(1000);
+//    vfd->WriteOneChar(0, 0x30);
+//    osDelay(5);
+//    vfd->Show();
+//    osDelay(5);
+    din->Toggle();
+    delay_us(1000);
+//    set_cursor(0, 0);
+//    clear_screen();
+
+//    led->Low();
+//    print("%ld\r\n", bsp::GetHighresTickMicroSec());
+//    osDelay(100);
   }
 }
