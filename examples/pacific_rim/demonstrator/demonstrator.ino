7017ef62402b964c508549167f0fce822a1fdbb1
Guang Yin <guangy2@illinois.edu>
2023-07-26 20:40:34 +0800

    [chore] add more servo

--- examples/pacific_rim/demonstrator/demonstrator.ino
+++ examples/pacific_rim/demonstrator/demonstrator.ino
@@ -1,22 +1,35 @@
 #include <Wire.h>
 #include <DFRobot_ADS1115.h>
 
-DFRobot_ADS1115 ads(&Wire);
-const int pwmPin = A4;
+DFRobot_ADS1115 ads1(&Wire);
+DFRobot_ADS1115 ads2(&Wire);
+
+const int pwm1Pin = A4;
+const int pwm2Pin = A3;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(pwmPin, OUTPUT);
-  ledcSetup(8, 50, 16);
-  ledcAttachPin(pwmPin, 8);
+  pinMode(pwm1Pin, OUTPUT);
+  pinMode(pwm2Pin, OUTPUT);
+  ledcSetup(0, 50, 16);
+  ledcSetup(1, 50, 16);
+  ledcAttachPin(pwm1Pin, 0);
+  ledcAttachPin(pwm2Pin, 1);
 
-  ads.setAddr_ADS1115(ADS1115_IIC_ADDRESS0);
-  ads.setGain(eGAIN_TWOTHIRDS);
-  ads.setMode(eMODE_SINGLE);
-  ads.setRate(eRATE_128);
-  ads.setOSMode(eOSMODE_SINGLE);
-  ads.init();
+  ads1.setAddr_ADS1115(ADS1115_IIC_ADDRESS0);
+  ads1.setGain(eGAIN_TWOTHIRDS);
+  ads1.setMode(eMODE_SINGLE);
+  ads1.setRate(eRATE_128);
+  ads1.setOSMode(eOSMODE_SINGLE);
+  ads1.init();
+
+  ads2.setAddr_ADS1115(ADS1115_IIC_ADDRESS1);
+  ads2.setGain(eGAIN_TWOTHIRDS);
+  ads2.setMode(eMODE_SINGLE);
+  ads2.setRate(eRATE_128);
+  ads2.setOSMode(eOSMODE_SINGLE);
+  ads2.init();
 }
 
 int calculatePWM(int degree) {
@@ -28,11 +41,14 @@ int calculatePWM(int degree) {
 // [16] 2880, 4940~5020, 7080
 
 void loop() {
-  if (ads.checkADS1115()) {
+  if (ads1.checkADS1115()) {
     int16_t adc0, adc1, adc2, adc3;
-    adc0 = ads.readVoltage(0);
-    Serial.println(adc0);
-    ledcWrite(8, calculatePWM(adc0 / 3297.0 * 360));
+    adc0 = ads1.readVoltage(0);
+    adc1 = ads1.readVoltage(1);
+    // Serial.println(adc0);
+    ledcWrite(0, calculatePWM(adc0 / 3297.0 * 360));
+    ledcWrite(1, calculatePWM(adc1 / 3297.0 * 360));
+
   } else
     Serial.println("ADS1115 Disconnected!");
 }
