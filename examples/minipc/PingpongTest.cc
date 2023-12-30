fc15c38446bb08efd8e61d19a717520d5a3cb69b
AzulRadio <50132891+AzulRadio@users.noreply.github.com>
2023-12-29 18:59:26 -0600

    Add minipc protocol 2.0 (#66)
    * Break Send() into Pack() for each message type
    
    * add function headers and helpers for Pack()
    
    * lossless float transmission
    
    * data parse rewrite
    
    * rename autoaim_protocol as minipc_protocol; add more comments.
    
    * add examples
    
    ---------
    
    Co-authored-by: Richard Xu <rickxu2@illinois.edu>

--- examples/minipc/PingpongTest.cc
+++ examples/minipc/PingpongTest.cc
@@ -27,7 +27,7 @@
 #include "bsp_print.h"
 #include "bsp_uart.h"
 #include "cmsis_os.h"
-#include "autoaim_protocol.h"
+#include "minipc_protocol.h"
 #include "rgb.h"
 
 #define RX_SIGNAL (1 << 0)
@@ -52,58 +52,44 @@ void RM_RTOS_Init(void) {
 void RM_RTOS_Default_Task(const void* argument) {
   UNUSED(argument);
 
-  uint32_t length;
-  uint8_t* data;
-
-  auto uart = std::make_unique<CustomUART>(&huart1);  // see cmake for which uart
+  auto uart = std::make_unique<CustomUART>(&huart1);
   uart->SetupRx(50);
   uart->SetupTx(50);
 
-  auto miniPCreceiver = communication::AutoaimProtocol();
-  int total_processed_bytes = 0;
+  auto minipc_session = communication::MinipcPort();
+
+  communication::gimbal_data_t gimbal_data;
+
+  const communication::status_data_t* status_data;
+
+  gimbal_data.rel_yaw = 100;
+  gimbal_data.rel_pitch = 200;
+  gimbal_data.debug_int = 50;
+  gimbal_data.mode = 1;
+
+  uint8_t packet_to_send[minipc_session.MAX_PACKET_LENGTH];
+  uint8_t *data;
+  int32_t length;
 
   while (true) {
     /* wait until rx data is available */
-    led->Display(0xFF0000FF);
+    //led->Display(0xFF0000FF);
 
-    // An alternative is to use osThreadFlagsWait.
-    // However, we want to experiment with periodic sending
-    uint32_t flags = osThreadFlagsGet();
+    // TX RX test. Use with communication/communicator.py in iRM_Vision_2023 repo
+    // In the communicator.py, need to set testing=Test.PINGPONG for this test
+
+    // Wait until first packet from minipc.
+    uint32_t flags = osThreadFlagsWait(RX_SIGNAL, osFlagsWaitAll, osWaitForever);
     if (flags & RX_SIGNAL) {
-      /* time the non-blocking rx / tx calls (should be <= 1 osTick) */
-
-      // max length of the UART buffer at 150Hz is ~50 bytes
+      // When packet received from miniPC, increase rel_pitch by 1 and send back
       length = uart->Read(&data);
-      total_processed_bytes += length;
-
-      miniPCreceiver.Receive(data, length);
-      uint32_t valid_packet_cnt = miniPCreceiver.get_valid_packet_cnt();
-
-      // Jetson / PC sends 200Hz valid packets for stress testing
-      // For testing script, please see iRM_Vision_2023/Communication/communicator.py
-      // For comm protocol details, please see iRM_Vision_2023/docs/comm_protocol.md
-      if (valid_packet_cnt == 1000) {
-        // Jetson test cases write 1000 packets. Pass
-        led->Display(0xFF00FF00);
-        osDelay(10000);
-        // after 10 seconds, write 1000 alternating packets to Jetson
-        communication::STMToJetsonData packet_to_send;
-        uint8_t my_color = 1; // blue
-        for (int i = 0; i < 1000; ++i) {
-          if (i % 2 == 0) {
-            my_color = 1; // blue
-          } else {
-            my_color = 0; // red
-          }
-          miniPCreceiver.Send(&packet_to_send, my_color, 0.5, 0.42, 0);
-          uart->Write((uint8_t*)&packet_to_send, sizeof(communication::STMToJetsonData));
-          // NOTE: THIS BREAKS WHEN WORKING AT 1000HZ!
-          osDelay(2);
-        }
-      }
-      // blue when nothing is received
-      led->Display(0xFF0000FF);
+      minipc_session.ParseUartBuffer(data, length);
+      status_data = minipc_session.GetStatus();
+      gimbal_data.rel_pitch = status_data->rel_pitch + 1;
+      gimbal_data.rel_yaw = status_data->rel_yaw;
+      minipc_session.Pack(packet_to_send, (void*)&gimbal_data, communication::GIMBAL_CMD_ID);
+      uart->Write(packet_to_send, minipc_session.GetPacketLen(communication::GIMBAL_CMD_ID));
     }
-    osDelay(2);
+    osDelay(10);
   }
 }
---
+++ shared/libraries/minipc_protocol.cc
@@ -0,0 +1,266 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2023 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#include "minipc_protocol.h"
+
+#include <cstring>
+#include <memory>
+
+namespace communication {
+
+// For definitions of constants, check out the documentation at
+// illini-robomaster/iRM_Vision_2023/docs/comm_protocol.md
+static constexpr uint8_t SEQNUM_OFFSET = 2;
+static constexpr uint8_t DATA_LENGTH_OFFSET = SEQNUM_OFFSET + 2;
+static constexpr uint8_t CMD_ID_OFFSET = DATA_LENGTH_OFFSET + 1;
+static constexpr uint8_t DATA_OFFSET = CMD_ID_OFFSET + 1;
+
+MinipcPort::MinipcPort() {
+  buffer_index_ = 0; // current pointer to write
+  valid_flag_ = 0;
+  seqnum_ = 0;
+}
+
+void MinipcPort::Pack(uint8_t* packet, void* data, uint8_t cmd_id) {
+  switch (cmd_id) {
+    case GIMBAL_CMD_ID:
+      PackGimbalData(packet, static_cast<gimbal_data_t*>(data));
+      break;
+    case COLOR_CMD_ID:
+      PackColorData(packet, static_cast<color_data_t*>(data));
+      break;
+    case CHASSIS_CMD_ID:
+      PackChassisData(packet, static_cast<chassis_data_t*>(data));
+      break;
+  }
+}
+
+void MinipcPort::PackGimbalData(uint8_t* packet, gimbal_data_t* data) {
+  AddHeaderTail(packet, GIMBAL_CMD_ID);
+  memcpy(&packet[0 + DATA_OFFSET], &data->rel_yaw, sizeof(float));
+  memcpy(&packet[4 + DATA_OFFSET], &data->rel_pitch, sizeof(float));
+  packet[8 + DATA_OFFSET] = data->mode;
+  packet[9 + DATA_OFFSET] = data->debug_int;
+  AddCRC8(packet, GIMBAL_CMD_ID);
+}
+
+void MinipcPort::PackColorData(uint8_t* packet, color_data_t* data) {
+  AddHeaderTail(packet, COLOR_CMD_ID);
+  packet[DATA_OFFSET] = data->my_color;
+  AddCRC8(packet, COLOR_CMD_ID);
+}
+
+void MinipcPort::PackChassisData(uint8_t* packet, chassis_data_t* data) {
+  AddHeaderTail(packet, CHASSIS_CMD_ID);
+  memcpy(&packet[0 + DATA_OFFSET], &data->vx, sizeof(float));
+  memcpy(&packet[4 + DATA_OFFSET], &data->vy, sizeof(float));
+  memcpy(&packet[8 + DATA_OFFSET], &data->vw, sizeof(float));
+  AddCRC8(packet, CHASSIS_CMD_ID);
+}
+
+uint8_t MinipcPort::GetPacketLen(uint8_t cmd_id) {
+  // Total length = Data length + header & tail (8 bytes) + crc checksum (1 byte)
+  return CMD_TO_LEN[cmd_id] + 9;
+}
+
+void MinipcPort::ParseUartBuffer(const uint8_t* data, int32_t length) {
+  // Four cases
+  // Case 1: everything is fresh with complete package(s)
+  // Case 2: everything is fresh; package is incomplete
+  // Case 3: package contains half previous package and half new package
+  // Case 4: package contains half previous package and new package(s)
+  int i = 0;
+  if (buffer_index_ > 0) {
+    // Case 3 and 4
+    // copy the remaining bytes from previous package
+    while (i < (int)length) {
+      possible_packet[buffer_index_] = data[i];
+      // Parse possible packet if detect tail
+      if (possible_packet[buffer_index_ - 1] == 'E' &&
+          possible_packet[buffer_index_]     == 'D') {
+        buffer_index_ = 0;
+        VerifyAndParseData();
+        break;
+      }
+      buffer_index_++;
+      i++;
+      // drop packet if buffer overflow
+      if (buffer_index_ >= MAX_PACKET_LENGTH) {
+        buffer_index_ = 0;
+        break;
+      }
+    }
+  }
+
+  while (i < (int)length) {
+    if (buffer_index_ == 0) {
+      if (i == (int)length - 1) {
+        // A special case to handle the last byte; buffer_index_ must be zero
+        if (data[i] == 'S') {
+          possible_packet[buffer_index_++] = data[i];
+        }
+        return;
+      }
+      if (data[i] == 'S' && data[i + 1] == 'T'){
+        // Detect packet head; start copying
+        possible_packet[buffer_index_++] = data[i++];
+        possible_packet[buffer_index_++] = data[i++];
+      } else {
+        i++;
+      }
+    } else {
+      possible_packet[buffer_index_] = data[i];
+      // Parse possible packet if detect tail
+      if (possible_packet[buffer_index_ - 1] == 'E' &&
+          possible_packet[buffer_index_]     == 'D') {
+        buffer_index_ = 0;
+        VerifyAndParseData();
+      } else {
+      // If not packet end, continue
+        buffer_index_++;
+        i++;
+        // drop packet if buffer overflow
+        if (buffer_index_ >= MAX_PACKET_LENGTH) {
+          buffer_index_ = 0;
+        }
+      }
+    }
+  }
+}
+
+uint8_t MinipcPort::GetCmdId(void) {
+    return cmd_id_;
+}
+
+const status_data_t* MinipcPort::GetStatus(void) {
+  return &status_;
+}
+
+uint8_t MinipcPort::GetValidFlag(void) {
+  uint8_t temp = valid_flag_;
+  valid_flag_ = 0;
+  return temp;
+}
+
+uint16_t MinipcPort::GetSeqnum(void) {
+  return seqnum_;
+}
+
+uint32_t MinipcPort::GetValidPacketCnt(void) {
+  return valid_packet_cnt_;
+}
+
+/**
+ * Every function below is a private function
+ */
+
+
+// 8 bytes
+void MinipcPort::AddHeaderTail (uint8_t* packet, uint8_t cmd_id) {
+  // Add header
+  packet[0] = 'S';
+  packet[1] = 'T';
+  // dummy seq num
+  packet[SEQNUM_OFFSET] = 0;
+  packet[SEQNUM_OFFSET + 1] = 0 >> 8;
+  packet[DATA_LENGTH_OFFSET] = CMD_TO_LEN[cmd_id];
+  packet[CMD_ID_OFFSET] = cmd_id;
+
+  // Add tail WITHOUT crc8
+  const int EOF_OFFSET = DATA_OFFSET + CMD_TO_LEN[cmd_id] + 1;
+  packet[EOF_OFFSET] = 'E';
+  packet[EOF_OFFSET + 1] = 'D';
+}
+
+// 1 bytes
+void MinipcPort::AddCRC8 (uint8_t* packet, int8_t cmd_id) {
+  const int CRC8_OFFSET = DATA_OFFSET + CMD_TO_LEN[cmd_id];
+  packet[CRC8_OFFSET] = get_crc8_check_sum(packet, CRC8_OFFSET, 0);
+}
+
+void MinipcPort::VerifyAndParseData(void) {
+  // TODO: implement thread-safe logic here (use a lock to handle changes from interrupt)
+  // here we can assume that the package is complete
+  // in the possible_packet buffer
+
+  // A minor issue with current implementation: imagine the following case:
+  //  Two packets arrive in two UART calls.
+  //  The first packet misses 1 byte, but the second one is complete.
+  //  In this case, when the possible_packet buffer is filled
+  //  (the last bytes are from the second packet), VerifyAndParseData() will be called. The whole buffer
+  //  would be tossed, resulting in two unusable packets. However, if we implement this logic, we would be
+  //  able to recover the second packet.
+
+  // This is a minor issue because
+  //    1) we don't observe this even when packets are sent at 200Hz
+  //    2) probability of this happening is very low. The second packet has to be sent in two slices to
+  //       trigger this issue. (first slice: S/T is sent to possible_packet; second slide: the rest)
+  // Note: The issue discussed above might be deprecated.
+
+  // if packet Head or Tail is corrupted
+  uint8_t cmd_id = possible_packet[CMD_ID_OFFSET];
+  if (possible_packet[0] != 'S' || possible_packet[1] != 'T' ||
+      possible_packet[CMD_TO_LEN[cmd_id] + HT_LEN - 2] != 'E' ||
+      possible_packet[CMD_TO_LEN[cmd_id] + HT_LEN - 1] != 'D') {
+    valid_flag_ = 0;
+    return;
+  }
+
+  // if packet crc8 checksum is valid
+  if (verify_crc8_check_sum(possible_packet, CMD_TO_LEN[cmd_id] + HT_LEN - 2)) {
+    ParseData(cmd_id);
+    valid_packet_cnt_++;
+    valid_flag_ = 1;
+  } else {
+    valid_flag_ = 0;
+  }
+}
+
+
+
+void MinipcPort::ParseData(uint8_t cmd_id) {
+  // Update member variable
+  cmd_id_ = cmd_id;
+
+  // Update seqnum
+  memcpy(&seqnum_, &possible_packet[SEQNUM_OFFSET], sizeof(uint16_t));
+
+  // Update data section
+  switch (cmd_id) {
+    case GIMBAL_CMD_ID:
+      memcpy(&(status_.rel_yaw),   &possible_packet[0 + DATA_OFFSET], sizeof(float));
+      memcpy(&(status_.rel_pitch), &possible_packet[4 + DATA_OFFSET], sizeof(float));
+      status_.mode = possible_packet[8 + DATA_OFFSET];
+      status_.debug_int = possible_packet[9 + DATA_OFFSET];
+      break;
+    case COLOR_CMD_ID:
+      status_.my_color = possible_packet[DATA_OFFSET];
+      break;
+    case CHASSIS_CMD_ID:
+      memcpy(&(status_.vx), &possible_packet[0 + DATA_OFFSET], sizeof(float));
+      memcpy(&(status_.vy), &possible_packet[4 + DATA_OFFSET], sizeof(float));
+      memcpy(&(status_.vw), &possible_packet[8 + DATA_OFFSET], sizeof(float));
+      break;
+  }
+}
+
+
+}  // namespace communication
+
