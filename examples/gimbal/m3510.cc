066f5cdaad2fe4a0a16c7b67793abf706ae87ecd
Richard Xu <rickxu2@illinois.edu>
2024-01-27 23:02:10 -0600

    3510 gimbal and restore 3510 pid example

--- examples/gimbal/m3510.cc
+++ examples/gimbal/m3510.cc
@@ -34,31 +34,27 @@
 #define KEY_GPIO_GROUP GPIOB
 #define KEY_GPIO_PIN GPIO_PIN_2
 
-#define CONTROLLER
-
 bsp::CAN* can1 = nullptr;                   /*CAN*/
 control::MotorCANBase* motor = nullptr;     /* 3510Motor*/
 
 // BoolEdgeDetector key_detector(false);    /* Edge Detector*/
 
-
-float pid_params[3]{1500,30,300};           /* PID Params */
-
-#ifdef CONTROLLER
+//for osDelay(1)
+float pid_params[3]{24000,0,850000};           /* PID Params */
+//20000, 0, 160000
+float angle_with_center_of_mass = 6.04; // the motor absolute encoder angle in radians that the CM is right below the motor axis
+float gravity_kF = 3000;  //gravity compensation coefficient
+ 
   remote::DBUS* dbus = nullptr;             /* Controller */
-#endif
 
 void RM_RTOS_Init() {
     print_use_uart(&huart1);                /* Print through *huart*/
 
-
     /* Init */
     can1 = new bsp::CAN(&hcan1, true);
-    motor =  new control::Motor3510(can1,0x205);
+    motor =  new control::Motor3510(can1,0x207);
 
-#ifdef CONTROLLER
     dbus = new remote::DBUS(&huart3);
-#endif
 
 
 }
@@ -68,44 +64,66 @@ void RM_RTOS_Default_Task(const void* args){
 
     /* PID Controller */
     control::ConstrainedPID pid;  
-    pid.Reinit(pid_params,3000,30000); 
+    pid.Reinit(pid_params, 3000, 30000); 
 
     /* Motors */
     control::MotorCANBase* motors[] = {motor};
     
-#ifdef CONTROLLER
     /* 500 ticks delay waiting for controller init */
     osDelay(500);
-#endif
 
     /* Params Initialization */
     float diff = 0; /* Position Difference */
     float out = 0;  /* PID Controller Output */
-    float target = 3.14; /* Target Position */
- 
+    float target = 2.75; /* Target Position */ //this angle when facing forwards
+    float target_angular_vel = 0; /*target m3510 angular_vel*/
 
+    //for timing
+    float curr_time_s = 0; //current timestamp
+    float last_time_s = HAL_GetTick() / 1000.0; //last loop's timestamp
+    float delta_time_s = 0; //loop delta time
+
+
+    int count = 0;
 
     while (true) {
-#ifdef CONTROLLER
-        target = clip<float>(float(dbus->ch1) / remote::DBUS::ROCKER_MAX * PI,-PI,PI);
-#endif
-
+        curr_time_s = HAL_GetTick() / 1000.0;
+        delta_time_s = curr_time_s - last_time_s;
+        last_time_s = curr_time_s;
+        target_angular_vel = float(dbus->ch1) / remote::DBUS::ROCKER_MAX * PI; //maximum 1 PI/s rate change
+        target += target_angular_vel * delta_time_s;
+        target = clip<float>(target,0,2*PI); //angle limit for the gimbal
+        
         //Calculate error
         diff = motor->GetThetaDelta(target);
         out = pid.ComputeOutput(diff);
 
+        //Feed Forward for gravity compensation
+        out += sin(motor->GetTheta() - angle_with_center_of_mass) * gravity_kF;
+
         //Control Motor 
         motor->SetOutput(out);
+
         control::MotorCANBase::TransmitOutput(motors, 1);
 
         //Check output
-        print("target: %.4f, diff: %.4f \r\n",target,diff);
-        motor->PrintData();
-#ifdef CONTROLLER
-        print("CH0: %-4d CH1: %-4d CH2: %-4d CH3: %-4d ", dbus->ch0, dbus->ch1, dbus->ch2, dbus->ch3);
-#endif
-        print("SWL: %d SWR: %d @ %d ms\r\n", dbus->swl, dbus->swr, dbus->timestamp);
-        osDelay(100);
+    
+        count ++;
+
+        if(count == 100){
+
+            set_cursor(0, 0);
+            clear_screen();
+            print("target: %.4f, diff in degree: %.4f, target_angular_vel: %.4f \r\n",target,diff/PI*180.0, target_angular_vel);
+            print("PID output: %.f \r\n", out);
+            motor->PrintData();
+            count = 0;
+            print("CH0: %-4d CH1: %-4d CH2: %-4d CH3: %-4d ", dbus->ch0, dbus->ch1, dbus->ch2, dbus->ch3);
+            print("SWL: %d SWR: %d @ %d ms\r\n", dbus->swl, dbus->swr, dbus->timestamp);
+            print("time: %f s", curr_time_s);
+        }
+
+        osDelay(1);
 
     }
 }
