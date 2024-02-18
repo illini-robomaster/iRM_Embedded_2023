a337d0e07516635771ffa648f2b4df9d435260d3
jerrywrx <jerrywrx@foxmail.com>
2024-02-17 18:05:28 -0600

    add 3 joints 4310 example in m4310_mit.cc

--- examples/motor/m4310_mit.cc
+++ examples/motor/m4310_mit.cc
@@ -1,22 +1,22 @@
 /****************************************************************************
-*                                                                          *
-*  Copyright (C) 2023 RoboMaster.                                          *
-*  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
-*                                                                          *
-*  This program is free software: you can redistribute it and/or modify    *
-*  it under the terms of the GNU General Public License as published by    *
-*  the Free Software Foundation, either version 3 of the License, or       *
-*  (at your option) any later version.                                     *
-*                                                                          *
-*  This program is distributed in the hope that it will be useful,         *
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
-*  GNU General Public License for more details.                            *
-*                                                                          *
-*  You should have received a copy of the GNU General Public License       *
-*  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
-*                                                                          *
-****************************************************************************/
+ *                                                                          *
+ *  Copyright (C) 2023 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
 
 #include "bsp_gpio.h"
 #include "bsp_print.h"
@@ -26,7 +26,9 @@
 #include "dbus.h"
 
 static bsp::CAN* can = nullptr;
-static control::Motor4310* motor = nullptr;
+static control::Motor4310* motor1 = nullptr;
+static control::Motor4310* motor2 = nullptr;
+static control::Motor4310* motor3 = nullptr;
 remote::DBUS* dbus = nullptr;
 
 void RM_RTOS_Init() {
@@ -41,7 +43,9 @@ void RM_RTOS_Init() {
    *  VEL: velocity mode  */
 
   /* Make sure motor is set to the correct mode (in helper tool). Otherwise, motor won't start */
-  motor = new control::Motor4310(can, 0x02, 0x01, control::MIT);
+  motor1 = new control::Motor4310(can, 0x02, 0x01, control::MIT);
+  motor2 = new control::Motor4310(can, 0x04, 0x03, control::MIT);
+  motor3 = new control::Motor4310(can, 0x06, 0x05, control::MIT);
   dbus = new remote::DBUS(&huart3);
 }
 
@@ -49,30 +53,49 @@ void RM_RTOS_Default_Task(const void* args) {
   /* press reset if no response */
   UNUSED(args);
 
-  control::Motor4310* motors[] = {motor};
+  control::Motor4310* motors[] = {motor1, motor2, motor3};
 
   while(dbus->swr != remote::DOWN){}  // flip swr to start
 
   /* Use SetZeroPos if you want to set current motor position as zero position. If uncommented, the
    * zero position is the zero position set before */
-  motor->SetZeroPos();
-  motor->MotorEnable();
+  motor1->SetZeroPos();
+  motor1->MotorEnable();
+  motor2->SetZeroPos();
+  motor2->MotorEnable();
+  motor3->SetZeroPos();
+  motor3->MotorEnable();
 
-  float pos = 0;
-  float min_pos = -PI/8;
-  float max_pos = PI/6;
+  float pos_1 = 0;
+  float pos_2 = 0;
+  float pos_3 = 0;
+  float min_pos = -PI;
+  float max_pos = PI;
   while (true) {
-    float vel;
-    vel = clip<float>(dbus->ch1 / 660.0 * 15.0, -15, 15);
-    pos += vel / 200;
-    pos = clip<float>(pos, min_pos, max_pos);   // clipping position within a range
+    float vel_1;
+    float vel_2;
+    float vel_3;
+
+    vel_1 = clip<float>(dbus->ch1 / 660.0 * 15.0, -15, 15);
+    pos_1 += vel_1 / 200;
+    pos_1 = clip<float>(pos_1, min_pos, max_pos);   // clipping position within a range
+
+    vel_2 = clip<float>(dbus->ch2 / 660.0 * 15.0, -15, 15);
+    pos_2 += vel_2 / 200;
+    pos_2 = clip<float>(pos_2, -PI/2, PI/2);   // clipping position within a range
+
+    vel_3 = clip<float>(dbus->ch3 / 660.0 * 15.0, -15, 15);
+    pos_3 += vel_3 / 200;
+    pos_3 = clip<float>(pos_3, min_pos, max_pos);   // clipping position within a range
 
     set_cursor(0, 0);
     clear_screen();
-    print("Vel Set: %f  Pos Set: %f\n", vel, pos);
+    print("Vel Set: %f  Pos Set: %f\n", vel_1, pos_1);
 
-    motor->SetOutput(pos, vel, 30, 0.5, 0);
-    control::Motor4310::TransmitOutput(motors, 1);
+    motor1->SetOutput(pos_1, vel_1, 30, 0.5, 0);
+    motor2->SetOutput(pos_2, vel_2, 30, 0.5, 0);
+    motor3->SetOutput(pos_3, vel_3, 30, 0.5, 0);
+    control::Motor4310::TransmitOutput(motors, 3);
     osDelay(10);
   }
 }
