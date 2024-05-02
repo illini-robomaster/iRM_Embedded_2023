77c536e7271e06f58072071b375947b12ae33f5a
jerrywrx <jerrywrx@foxmail.com>
2024-05-02 12:28:38 -0500

    added mc01 arm example

--- examples/arm/typeC.cc
+++ examples/arm/typeC.cc
@@ -25,6 +25,7 @@
 #include "motor.h"
 #include "dbus.h"
 #include "sbus.h"
+#include "utils.h"
 
 #define SBUS_START_IDX 5
 #define WINDOW_SIZE 50
@@ -37,7 +38,7 @@ static control::Motor4310* motor3 = nullptr;
 //static remote::DBUS* dbus = nullptr;
 static remote::SBUS* sbus = nullptr;
 static bsp::GPIO* key = nullptr;
-static float sbus_prev[6] = {0, 0, 0, 0, 0, 0};
+//static float sbus_prev[6] = {0, 0, 0, 0, 0, 0};
 static int sbus_prev_2d[6][WINDOW_SIZE] = {{0}};
 static float sbus_avg[6] = {0, 0, 0, 0, 0, 0};
 
@@ -79,22 +80,32 @@ void RM_RTOS_Default_Task(const void* args) {
  motor3->SetZeroPos();
  motor3->MotorEnable();
 
- float pos_1 = 0.0;
- float pos_2 = 0.0;
- float pos_3 = 0.0;
- float vel_1 = 0.0;
- float vel_2 = 0.0;
- float vel_3 = 0.0;
+ float J4_pos = 0.0;
+ float J5_pos = 0.0;
+ float J6_pos = 0.0;
+ UNUSED(J5_pos);
 
- float min_pos1 = -PI/2;
- float max_pos1 = PI/2;
- float min_pos2 = -PI/2;
- float max_pos2 = PI/2;
- float min_pos3 = -PI/2;
- float max_pos3 = PI/2;
+ float J4_min = -PI;
+ float J4_max =  PI;
+ float J5_min = -PI;
+ float J5_max =  PI;
+ float J6_min = -PI;
+ float J6_max =  PI;
 
- float impulse_filtered[6] = {0, 0, 0, 0, 0, 0};
- float sbus_derivative[6] = {0, 0, 0, 0, 0, 0};
+// float impulse_filtered[6] = {0, 0, 0, 0, 0, 0};
+// float sbus_derivative[6] = {0, 0, 0, 0, 0, 0};
+ std::vector<float> filtered_signal;
+ std::deque<int> filter_window;
+ float filtered_output = 0;
+ int window_size = 5;
+
+ // initialize previous sbus values to avoid discrete jumps
+ for (int i = SBUS_START_IDX; i < SBUS_START_IDX + 6; i++) {
+   int index = i - SBUS_START_IDX;  // Adjust index for zero-based array indexing
+   for (int j = 0; j < WINDOW_SIZE; j++) {
+     sbus_prev_2d[index][j] = sbus->ch[i];
+   }
+ }
 
  while (true) {
    print("test\r\n");
@@ -105,7 +116,7 @@ void RM_RTOS_Default_Task(const void* args) {
      for (int j = 0; j < WINDOW_SIZE - 1; j++) {
        sbus_prev_2d[index][j] = sbus_prev_2d[index][j + 1];
      }
-     sbus_prev_2d[index][WINDOW_SIZE-1] = sbus->ch[i];
+     sbus_prev_2d[index][WINDOW_SIZE - 1] = sbus->ch[i];
 
      float sum = 0;
      for (int j = 0; j < WINDOW_SIZE; j++) {
@@ -114,52 +125,28 @@ void RM_RTOS_Default_Task(const void* args) {
      sbus_avg[index] = sum / static_cast<float>(WINDOW_SIZE);
    }
 
-   print("test2\r\n");
-   /* Impulse Filter */
-   for (int i = SBUS_START_IDX; i < SBUS_START_IDX + 6; i++) {
-     int index = i - SBUS_START_IDX;  // Adjust index for zero-based array indexing
-     impulse_filtered[index] = sbus_avg[index];
-
-     // impulse filtering
-     sbus_derivative[index] = (sbus_avg[index] - sbus_prev[index]);  // TODO: check range
-     if (abs(sbus_derivative[index]) > 30.0) {
-       impulse_filtered[index] = sbus_prev[index];  // use previous value if impulse is too high
-     } else {
-       sbus_prev[index] = sbus_avg[index];
-     }
-     // update previous value only if signal is within range
-     //     if (abs(sbus->ch[i]) < FILTER_THRESHOLD) sbus_prev[index] = sbus->ch[i];
-   }
-
 //   for (int i = SBUS_START_IDX; i < SBUS_START_IDX + 6; i++) {
 //     int index = i - SBUS_START_IDX;  // Adjust index for zero-based array indexing
-//     sbus_filtered[index] = impulse_filter(sbus->ch[i], &sbus_prev[index], SBUS_THRESHOLD);
+//     MedianFilter(sbus->ch[i], filtered_signal, 5);
 //   }
 
-   vel_1 = clip<float>(impulse_filtered[5] / 680.0 * PI/2, -PI/2, PI/2);
-   pos_1 += vel_1 / 300;
-   pos_1 = clip<float>(pos_1, min_pos1, max_pos1);   // clipping position within a range
+   updateFilter(sbus->ch[10], filter_window, window_size, filtered_output);
 
-   vel_2 = clip<float>(impulse_filtered[6] / 680.0 * PI/2, -PI/2, PI/2);
-   pos_2 += vel_2 / 300;
-   pos_2 = clip<float>(pos_2, min_pos2, max_pos2);   // clipping position within a range
-
-   vel_3 = clip<float>(impulse_filtered[7] / 680.0 * PI/2, -PI/2, PI/2);
-   pos_3 += vel_3 / 300;
-   pos_3 = clip<float>(pos_3, min_pos3, max_pos3);   // clipping position within a range
+   J4_pos = clip<float>(sbus_avg[3] / 660.0 * PI, J4_min, J4_max);
+   J5_pos = clip<float>(sbus_avg[4] / 660.0 * PI, J5_min, J5_max);
+   J6_pos = clip<float>(sbus_avg[5] / 660.0 * PI, J6_min, J6_max);
 
    set_cursor(0, 0);
    clear_screen();
 //   print("d: %-4d, %-4d, %-4d, %-4d, %-4d, %-4d\n", sbus->ch[10], sbus->ch[11], sbus->ch[12], sbus->ch[13], sbus->ch[14], sbus->ch[15]);
-   print("d: %-4d, %-4f, %-4f, %-4f\n", sbus->ch[5], sbus_avg[0], sbus_derivative[0], impulse_filtered[0]);
+   print("d: %-4d, %-4f, %-4f, %-4f\n", sbus->ch[10], J4_pos, J5_pos, J6_pos);
 //   print("d: %-4d, %-4d, %-4d\n", sbus->ch[0], sbus_actual, sbus_derivative);
 //   print("d: %-4d, %-4d, %-4d\n", sbus->ch[1], sbus_actual2, sbus_derivative2);
 
    //   print("Vel Get: %f,  Pos Get: %f\n", vel_get, pos_get);
-
-//   motor1->SetOutput(vel_1, float(sbus_filtered[0]), 10, 0.5, 0);
-//   motor2->SetOutput(vel_2, float(sbus_filtered[0]), 10, 0.5, 0);
-//   motor3->SetOutput(vel_3, vel_3, 10, 0.5, 0);
+   motor1->SetOutput(J4_pos, J4_pos, 10, 0.5, 0);
+   motor2->SetOutput(J5_pos, J5_pos, 10, 0.5, 0);
+   motor3->SetOutput(J6_pos, J6_pos, 10, 0.5, 0);
    control::Motor4310::TransmitOutput(motors, 3);
    osDelay(10);
  }
