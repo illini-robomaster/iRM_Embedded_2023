0e6db738da8b0927f814c24383fbb6444194aeee
rickxu2 <rickxu2@illinois.edu>
2024-06-22 16:05:37 +0800

    fixed trapezoid_profile

--- shared/libraries/trapezoid_profile.h
+++ shared/libraries/trapezoid_profile.h
@@ -9,30 +9,30 @@ class TrapezoidProfile
 {
     private:
         // Kinematics Constraints
-        float acceleration;
-        float cruise_velocity;
+        float acc;
+        float cruise_vel;
 
         // kinematics states
-        kinematics_state current_state;
-        kinematics_state goal_state;
+        kinematics_state m_current_state;
 
         // estimated critical timestamps
         float end_acc_time;
         float end_cruise_time;
         float end_decc_time; 
 
-        float last_update_time_ms;
-
         int direction;
 
     public:
-        TrapezoidProfile(float acc, float cruise_vel, float init_pos, int init_time_ms) : acceleration(acc), cruise_velocity(abs(cruise_vel)), last_update_time_ms(init_time_ms){}
+        TrapezoidProfile(float acceleration, float cruise_velocity) : acc(acceleration), cruise_vel(abs(cruise_velocity)){}
         
         /**
         * @brief update the internal target and return a new target that satisfy the acceleration and velocity limit.
+        * @param new_target the new target position, if want to keep the current target, set this to the current target.
+        * @param delta_t_ms the time that you want to advance to the next tick, can be the typical loop time.
+        * @param current_state the current state of the system, which contains position and velocity. Could be the last output of this function.
         * @return kinematics_state which contains position and velocity of the current setpoint at time t.
         */
-        kinematics_state calculate(float new_target, int current_time_ms);
+        kinematics_state calculate(float new_target, int current_time_ms, kinematics_state current_state);
 
         /**
          * @brief make sure the direction of the output state matches the 
